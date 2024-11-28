0c58347b4d9bdb987fcd7b2578d12d8292987c2d
rinjugatla <rin.jugatla@gmail.com>
2024-11-29 00:09:36 +0900

    関係者情報を追加

---
+++ web/src/lib/members.ts
@@ -0,0 +1,76 @@
+/**
+ * 運営陣
+ */
+export const HIBISCUS_CUP_SPONSORS = [
+    { role: '主催', name: 'りえぺこ', twitch: 'riepeko', twitch_id: '277818090', twitter: 'Riepeko_yolo' },
+    { role: '企画', name: 'だいん', twitch: '', twitch_id: '', twitter: 'DAiN87006586' },
+    { role: '運営', name: 'AiharaGames', twitch: 'aiharagames', twitch_id: '174564082', twitter: 'AiharaGames' },
+    { role: '運営', name: 'kshrrr', twitch: '', twitch_id: '', twitter: '' },
+    { role: '運営', name: 'たつもと', twitch: '', twitch_id: '', twitter: '' },
+    { role: '運営', name: 'しゃるおじ', twitch: '', twitch_id: '', twitter: '' },
+    { role: '運営', name: 'Gigu_zz', twitch: '', twitch_id: '', twitter: '' },
+    { role: 'ロゴイラスト', name: 'ゆんま', twitch: 'yunma_flw', twitch_id: '771305469', twitter: 'Yuika_flw' },
+]
+
+/**
+ * 配信者
+ */
+export const HIBISCUS_CUP_STREAMERS = [
+    {team: '本配信', name: 'りえぺこ', twitch: 'riepeko', twitch_id: '277818090', twitter: 'Riepeko_yolo', archive_ids: [], digest_ids: [], clip_ids: []},
+    
+    { team: 'A', name: 'せかめん', twitch: 'sekamenz', twitch_id: '927242788', twitter: 'sekamen_games', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'A', name: '御握利もぐ', twitch: 'mogu_vtuber', twitch_id: '776978805', twitter: 'Onigiri_MoguV', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'A', name: 'ちーちゃんん', twitch: 'chi_chaan', twitch_id: '644642707', twitter: 'chi_chaan_tw', archive_ids: [], digest_ids: [], clip_ids: [] },
+    { team: 'A', name: 'DEIcecreeeam', twitch: 'deicecreeeam', twitch_id: '234653657', twitter: 'deicecreeeam', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'A', name: '水玉あお', twitch: 'mizutamaao', twitch_id: '798055535', twitter: 'mizutamaao', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'A', name: 'ちゃま', twitch: 'chamachanzako', twitch_id: '777197488', twitter: 'lalalachiisanFB', archive_ids: [], digest_ids: [], clip_ids: []},
+
+    { team: 'B', name: '蒼音カノン', twitch: 'aone_kanon', twitch_id: '682441785', twitter: 'aone_kanon', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'B', name: 'ゲームする女', twitch: 'sumi_oy', twitch_id: '782185458', twitter: 'game_onna', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'B', name: 'うにチャン', twitch: 'uni03_', twitch_id: '727048434', twitter: 'uni_dane', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'B', name: 'しゅーたー', twitch: 'shootingmoon', twitch_id: '125575465', twitter: 'shooting_moon07', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'B', name: 'みやまるだよ', twitch: 'miyamarumaru', twitch_id: '216686393', twitter: 'Toricha___n', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'B', name: '猫研あいお', twitch: 'aionekotogi', twitch_id: '908263241', twitter: 'nekotogiaio', archive_ids: [], digest_ids: [], clip_ids: []},
+
+    { team: 'C', name: 'らっぴん', twitch: 'rappinttv', twitch_id: '157842387', twitter: 'rappinttv', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'C', name: '相生あい', twitch: 'aioi_ai', twitch_id: '133061268', twitter: 'AioiAi_v', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'C', name: 'うっきーな', twitch: 'ukki_nahasaru', twitch_id: 'ukkiina', twitter: '608957101', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'C', name: 'るぴぃ', twitch: 'iamrupi', twitch_id: '728189924', twitter: 'iamrupii', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'C', name: '白妙とき', twitch: 'shirotaetoki', twitch_id: '1051350049', twitter: 'RAG_Toki', archive_ids: [], digest_ids: [], clip_ids: []},
+    { team: 'C', name: 'おひつじめぇ', twitch: 'ohitsu_zi', twitch_id: '449244936', twitter: 'ohi_mee', archive_ids: [], digest_ids: [], clip_ids: []},
+]
+
+/**
+ * チーム情報
+ */
+export const HIBISCUS_CUP_TEAMS = [
+    {
+        team: '本配信',
+        twitter_link: 'https://x.com/Riepeko_yolo/status/1857710327695118537',
+        description: ''
+    },
+    {
+        team: 'A',
+        twitter_link: 'https://x.com/Riepeko_yolo/status/1857710335177929189',
+        description: `前回見事なチームの統率を魅せ、フラッグを持ち帰った
+せかめんさん、ハイビ杯常連優勝経験有のDeicecreeeamさん率いるAチーム
+素晴らしいチームワークを魅せてくれる事間違いなし！？`
+    },
+    {
+        team: 'B',
+        twitter_link: 'https://x.com/Riepeko_yolo/status/1857710341934952511',
+        description: `前回、策士な一面を魅せてくれたカノンさんと
+初参戦、たるこふの知識はお任せあれ！
+しゅーたーさん率いるBチーム、
+元気いっぱい！素敵なコンビガールズと期待の新人ちゃんが
+楽しい活躍の一面を見せてくれるハズ！`
+    },
+    {
+        team: 'C',
+        twitter_link: 'https://x.com/Riepeko_yolo/status/1857710348129976429',
+        description: `初参戦！めちゃ強ｵﾓﾛ！ならっぴんさん率いる
+kappa&Lab girls Cチーム！
+前回悪魔討伐を果たしたおひつじめぇ
+今回も狙うは,エネミー！？`
+    }
+]
